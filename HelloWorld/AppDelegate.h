0a94b6590a3c9c3082d452e7650a343515f21b6f
Andrew <Zhuk@Mac-mini-Andrew.local>
2014-08-11 15:22:57 +0300

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Andrew on 8/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Andrew on 8/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
